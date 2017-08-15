44b9833f8b2fb912e560b7a426d881cb8c456454
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-15 12:43:24 +0300

    Changed gui to only VUmeter

--- source/mathreverbparamids.h
+++ source/mathreverbparamids.h
@@ -3,7 +3,7 @@
 enum
 {
 	/** parameter ID */
-	kGainId = 0,	///< for the gain value (is automatable)
-	kVuPPMId,		///< for the Vu value return to host (ReadOnly parameter for our UI)
+	kGainId,	///< for the gain value (is automatable)
+	kVuPPMId = 0,		///< for the Vu value return to host (ReadOnly parameter for our UI)
 	kBypassId		///< Bypass value (we will handle the bypass process) (is automatable)
 };
