02324c447012e301521c7b6db9f5d7f4c36d18ef
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-16 12:59:03 +0300

    Eddit Volume meter

--- source/mathreverbprocess.h
+++ source/mathreverbprocess.h
@@ -6,7 +6,7 @@ namespace Vst {
 //------------------------------------------------------------------------
 template <typename SampleType>
 SampleType MathReverb::processAudio (SampleType** in, SampleType** out, int32 numChannels, int32 sampleFrames) {
-	SampleType vuPPM = 0;
+	SampleType vuPPM = 0, tmp;
 
 	// in real Plug-in it would be better to do dezippering to avoid jump (click) in gain value
 	for (int32 i = 0; i < numChannels; i++)
@@ -15,7 +15,9 @@ SampleType MathReverb::processAudio (SampleType** in, SampleType** out, int32 nu
 		SampleType* ptrIn = (SampleType*)in[i];
 		SampleType* ptrOut = (SampleType*)out[i];
 		while (--samples >= 0) {
-			vuPPM = (*ptrOut++) = (*ptrIn++);
+			tmp = (*ptrOut++) = (*ptrIn++);
+			if (tmp > vuPPM)
+				vuPPM = tmp;
 		}
 	}
 	return vuPPM;
