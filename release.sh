862e6a61bb663b01c67277891be703b767becb27
Dubrovin Egor <dubrovin.en@gmail.com>
2017-08-25 00:02:40 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -26,9 +26,9 @@ fi
 # Build project
 powershell "msbuild.exe vst3sdk\build\public.sdk\samples\vst\mathreverb\mathreverb.vcxproj /t:Build /p:Configuration=Release"
 
-pwd
 # Move result to destanation
-mv vst3sdk/build/VST3/Release/mathreverb.vst3 "$mvdir"./
+ls vst3sdk/build/VST3/Release#/mathreverb.vst3 "$mvdir"./
+ls "$mvdir"./
 
 # Destroy environment if need to
 if [ "$destroy" ]; then
