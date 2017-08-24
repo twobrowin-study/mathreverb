1b6148dd1ef04d3aefe293bf6e2b19166c9ad66c
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-24 21:46:58 +0300

    Changes in varibles meaning

--- release.sh
+++ release.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Varables section
-cmake_clean="./"
-mvdir="./"
-destroy="./"
-
-# Keys section
+# Keys and varibles section
 while [ -n "$1" ]; do
   case "$1" in
-    -r) cmake_clean="rm -r vst3sdk/build/*; cmake.exe -G'Visual Studio 14 2015 Win64' ../";;
+    -r) cmake_clean=true;;
+    -d) destroy=true;;
     -m) mvdir="/c/'Program Files'/'Common Files'/VST3/";;
-    -d) destroy="./destroy_env.sh";;
   esac
   shift
 done
 
 # Clean up build directory if need to
-"$cmake_clean"
+if [ "$cmake_clean" ]; then
+  rm -r vst3sdk/build/*
+  cmake.exe -G'Visual Studio 14 2015 Win64' ../
+fi
 
 # Create environment if need to
 if [ ! -d vst3sdk/public.sdk/samples/vst/mathreverb ]; then
@@ -30,4 +28,6 @@ powershell "msbuild.exe vst3sdk\build\public.sdk\samples\vst\mathreverb\mathreve
 mv VST3/Release/mathreverb.vst3 "$mvdir"mathreverb.vst3
 
 # Destroy environment if need to
-"$destroy"
+if [ "$destroy" ]; then
+  ./destroy_env.sh
+fi
