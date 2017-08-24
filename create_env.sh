42f8392b69e9c16e35d63824c870fc8ebea64321
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-24 22:09:12 +0300

    Add dirs test

--- create_env.sh
+++ create_env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 git clone --recursive https://github.com/steinbergmedia/vst3sdk.git
-mv mathreverb/ vst3sdk/public.sdk/samples/vst/
-cd vst3sdk
-echo >> CMakeLists.txt
-echo "# MathReverb plugin" >> CMakeLists.txt
-echo "add_subdirectory(public.sdk/samples/vst/mathreverb)" >> CMakeLists.txt
-mkdir build
-cd build
-cmake.exe -G"Visual Studio 14 2015 Win64" ../
+if [ "$(ls -A vst3sdk/base)" ] && [ "$(ls -A vst3sdk/cmake)" ] && [ "$(ls -A vst3sdk/pluginterfaces)" ] && [ "$(ls -A vst3sdk/public.sdk)" ] && [ "$(ls -A vst3sdk/vstgui4)" ]; then
+  mv mathreverb/ vst3sdk/public.sdk/samples/vst/
+  echo >> vst3sdk/CMakeLists.txt
+  echo "# MathReverb plugin" >> vst3sdk/CMakeLists.txt
+  echo "add_subdirectory(public.sdk/samples/vst/mathreverb)" >> vst3sdk/CMakeLists.txt
+  mkdir vst3sdk/build
+  cd vst3sdk/build
+  cmake.exe -G"Visual Studio 14 2015 Win64" ../
+  cd ../../
+else
+  echo "https://github.com/steinbergmedia/vst3sdk.git is not responding"
+fi
