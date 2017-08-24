641e39045814c9f66f01d4f73d473ea7e48dec4f
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-24 21:26:01 +0300

    Update mvdir varible

--- release.sh
+++ release.sh
@@ -4,15 +4,15 @@
 mvdir="../../"
 while [ -n "$1" ]; do
   case "$1" in
-    -r) build_clean="rm -r vst3sdk/build/*; cmake.exe -G'Visual Studio 14 2015 Win64' ../";;
+    -r) cmake_clean="rm -r vst3sdk/build/*; cmake.exe -G'Visual Studio 14 2015 Win64' ../";;
     -m) mvdir="/c/'Program Files'/'Common Files'/VST3";;
-    -d) dstr="./destroy_env.sh";;
+    -d) destroy="./destroy_env.sh";;
   esac
   shift
 done
 
 # Clean up build directory if need to
-"$build_clean"
+"$cmake_clean"
 
 # Create environment if need to
 if [ ! -d vst3sdk/public.sdk/samples/vst/mathreverb ]; then
@@ -24,7 +24,7 @@ cd vst3sdk/build
 powershell "msbuild.exe public.sdk\samples\vst\mathreverb\mathreverb.vcxproj /t:Build /p:Configuration=Release"
 
 # Move result to destanation
-mv VST3/Release/mathreverb.vst3 mathreverb.vst3
+mv VST3/Release/mathreverb.vst3 "$mvdir"/mathreverb.vst3
 
 # Destroy environment if need to
-"$dstr"
+"$destroy"
