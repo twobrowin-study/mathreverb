751f1a6b83890fbdf25caba646246b045cf6c8eb
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-25 10:45:59 +0300

    CMake recompile fix

--- destroy_env.sh
+++ destroy_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mv vst3sdk/public.sdk/samples/vst/mathreverb mathreverb_source
 rm -rf vst3sdk/build
+sed -i '/# MathReverb plugin/d' CMakeLists.txt
+sed -i '/add_subdirectory(public.sdk/samples/vst/mathreverb)/d' CMakeLists.txt
