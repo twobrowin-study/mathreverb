516717e803901c7eb3aebd0b1499cd8d310d3eb1
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-25 22:30:54 +0300

    Add git bilding for mathrevrb_source

--- create_env.sh
+++ create_env.sh
@@ -2,6 +2,12 @@
 echo "Moving mathreverb_source folder into vst3sdk/public.sdk/samples/vst/ as mathreverb"
 mv mathreverb_source vst3sdk/public.sdk/samples/vst/mathreverb
 
+echo "Changing vst3sdk/public.sdk/samples/vst/mathreverb/.git repository binding"
+sed -i '\%gitdir: ../.git/modules/mathreverb_source%c\gitdir: ../../../../../.git/modules/mathreverb_source' vst3sdk/public.sdk/samples/vst/mathreverb/.git
+
+echo "Changing .git/modules/mathreverb_source/config repository binding"
+sed -i '\%worktree = ../../../mathreverb_source%c\worktree = ../../../vst3sdk/public.sdk/samples/vst/mathreverb' .git/modules/mathreverb_source/config
+
 echo "Adding empty line into vst3sdk/CMakeLists.txt"
 echo >> vst3sdk/CMakeLists.txt
 
--- destroy_env.sh
+++ destroy_env.sh
@@ -2,6 +2,12 @@
 echo "Moving vst3sdk/public.sdk/samples/vst/mathreverb folder into ./ as mathreverb_source"
 mv vst3sdk/public.sdk/samples/vst/mathreverb mathreverb_source
 
+echo "Changing mathreverb_source/.git repository binding"
+sed -i '\%gitdir: ../../../../../.git/modules/mathreverb_source%c\gitdir: ../.git/modules/mathreverb_source' mathreverb_source/.git
+
+echo "Changing .git/modules/mathreverb_source/config repository binding"
+sed -i '\%worktree = ../../../vst3sdk/public.sdk/samples/vst/mathreverb%c\worktree = ../../../mathreverb_source' .git/modules/mathreverb_source/config
+
 echo "Removing '# MathReverb plugin' line from vst3sdk/CMakeLists.txt"
 sed -i '\%# MathReverb plugin%d' vst3sdk/CMakeLists.txt
 
