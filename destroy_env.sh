b3d905278972702e503dd8e1d0125c60b5076ca9
Egor Dubrovin <dubrovin.en@gmail.com>
2017-08-25 14:45:22 +0300

    Env destr rm fix

--- destroy_env.sh
+++ destroy_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mv vst3sdk/public.sdk/samples/vst/mathreverb mathreverb_source
-rm -rf vst3sdk/build
 sed -i '\%# MathReverb plugin%d' vst3sdk/CMakeLists.txt
 sed -i '\%add_subdirectory(public.sdk/samples/vst/mathreverb)%d' vst3sdk/CMakeLists.txt
+rm -rf vst3sdk/build
