eb73b4d38f6ce0af33dff9a2a45725677f882b02
Dubrovin Egor <dubrovin.en@gmail.com>
2017-11-11 16:07:19 +0300

    Update destroy_env.sh

--- destroy_env.sh
+++ destroy_env.sh
@@ -8,11 +8,5 @@ sed -i '\%gitdir: ../../../../../.git/modules/mathreverb_source%c\gitdir: ../.gi
 echo "Changing .git/modules/mathreverb_source/config repository binding"
 sed -i '\%worktree = ../../../vst3sdk/public.sdk/samples/vst/mathreverb%c\worktree = ../../../mathreverb_source' .git/modules/mathreverb_source/config
 
-echo "Removing '# MathReverb plugin' line from vst3sdk/CMakeLists.txt"
-sed -i '\%# MathReverb plugin%d' vst3sdk/CMakeLists.txt
-
-echo "Removing 'add_subdirectory(public.sdk/samples/vst/mathreverb)' line from vst3sdk/CMakeLists.txt"
-sed -i '\%add_subdirectory(public.sdk/samples/vst/mathreverb)%d' vst3sdk/CMakeLists.txt
-
 echo "Removing vst3sdk/build directory"
 rm -rf vst3sdk/build
